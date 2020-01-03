73e6aaf4eb5fe02e0dc9f0eb52a7a05bee012892
Kim <kim.kokubun@gmail.com>
2020-01-03 13:29:33 -0300

    Testing back cameras

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 // Set constraints for the video stream
-var constraints = { video: { facingMode: "user" }, audio: false };// Define constants
+var constraints = { video: { facingMode: "user" }, audio: false };
+
 const cameraView = document.querySelector("#camera--view"),
     cameraOutput = document.querySelector("#camera--output"),
     cameraSensor = document.querySelector("#camera--sensor"),
-    cameraTrigger = document.querySelector("#camera--trigger")// Access the device camera and stream to cameraView
+    cameraTrigger = document.querySelector("#camera--trigger"),
+    cameraSwitch = document.querySelector"camera--switch")
 function cameraStart() {
     navigator.mediaDevices
         .getUserMedia(constraints)
@@ -14,12 +16,15 @@ function cameraStart() {
     .catch(function(error) {
         console.error("Oops. Something is broken.", error);
     });
-}// Take a picture when cameraTrigger is tapped
+}
 cameraTrigger.onclick = function() {
     cameraSensor.width = cameraView.videoWidth;
     cameraSensor.height = cameraView.videoHeight;
     cameraSensor.getContext("2d").drawImage(cameraView, 0, 0);
     cameraOutput.src = cameraSensor.toDataURL("image/webp");
     cameraOutput.classList.add("taken");
-};// Start the video stream when the window loads
+};
+cameraSwitch.onclick = function(){
+    constraints = { video: { facingMode: "environment" }, audio: false };
+}
 window.addEventListener("load", cameraStart, false);
